639c6d83ee1cfa2c1fe32d547900e6170b1bcff8
MaBadiDen <kaba4ok.den@gmail.com>
2023-02-16 10:47:56 +0300

    ready

---
+++ src/main/java/pro/sky/telegrambot/entity/Notification.java
@@ -0,0 +1,51 @@
+package pro.sky.telegrambot.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private long userId;
+    private String message;
+    private LocalDateTime dataTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getDataTime() {
+        return dataTime;
+    }
+
+    public void setDataTime(LocalDateTime dataTime) {
+        this.dataTime = dataTime;
+    }
+}
